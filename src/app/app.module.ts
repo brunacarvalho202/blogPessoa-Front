a8cfe28b447bec4370e2f8cb3e468b3e42b66bb4
Bruna Bezerra <brubslubs2019@gmail.com>
2022-09-05 10:04:51 -0300

    componentes criados

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MenuComponent } from './menu/menu.component';
+import { LoginComponent } from './login/login.component';
+import { RodapeComponent } from './rodape/rodape.component';
+import { CadastroComponent } from './cadastro/cadastro.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
+    LoginComponent,
+    RodapeComponent,
+    CadastroComponent
   ],
   imports: [
     BrowserModule,
