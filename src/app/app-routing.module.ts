e56eba4004b3092c8b59ed9ffe1fa07ccdb38e29
Bruna Bezerra <brubslubs2019@gmail.com>
2022-09-08 21:58:43 -0300

    atualização dos componentes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,14 +2,16 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CadastroComponent } from './cadastro/cadastro.component';
 import { LoginComponent } from './login/login.component';
+import { MenuComponent } from './menu/menu.component';
+import { RodapeComponent } from './rodape/rodape.component';
 
 const routes: Routes = [
   {path: '', redirectTo: 'login', pathMatch: 'full'},
 
   {path: 'login', component: LoginComponent},
   {path: 'cadastro', component: CadastroComponent},
-  
-
+  {path: 'menu', component: MenuComponent},
+  {path: 'rodape', component: RodapeComponent}
 ];
 
 @NgModule({
